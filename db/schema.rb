cc18369683baac7860f9224a63f042fd8d4a055e
zenfumi <ys00878@gmail.com>
2020-08-24 16:38:53 +0900

    外部キー追加、モデル間の関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_083729) do
+ActiveRecord::Schema.define(version: 2020_08_24_005843) do
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "purpose"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_08_05_083729) do
     t.binary "spot_photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
