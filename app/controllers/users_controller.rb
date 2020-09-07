018993766df21c0a227e45aac8c57914bd9a3b8d
zenfumi <ys00878@gmail.com>
2020-09-07 14:56:19 +0900

    fix _header view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    #binding.pry
     @reviews = @user.reviews.paginate(page: params[:page])
   end
 end
