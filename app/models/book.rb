a8e21e7792af9989e3a600e48ba007b37668709c
zenfumi <ys00878@gmail.com>
2020-09-19 09:31:19 +0900

    change writing way about book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,6 @@
 class Book < ApplicationRecord
   has_one_attached :image
-  validates :title, presence: true
-  validates :title, length: { maximum: 50}
-  validates :author, presence: true
-  validates :author, length: { maximum: 30}
+  validates :title, presence: true, length:{ maximum: 50}
+  validates :author, presence: true, length:{ maximum: 30}
   validates :image, presence: true
 end
