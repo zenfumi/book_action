84c353c8b6db2f9844850b7ccf039c414037afab
zenfumi <ys00878@gmail.com>
2020-09-20 22:20:51 +0900

    change route and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :books
-  root 'static_pages#home'
+  root 'books#index'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
