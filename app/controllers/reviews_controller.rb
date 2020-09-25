e08783ea259dba14eaf3be2425dfacc4bc7c5ae2
zenfumi <ys00878@gmail.com>
2020-09-25 23:56:17 +0900

    fix routing and fix view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :correct_user, only: [:destroy, :edit]
   before_action :authenticate_user!,{only:[:edit, :update]}
-  #試行中
-  # before_action :set_books, only: [:new]
+  before_action :set_books, only: [:new, :create, :edit, :update]
 
   def index
     @reviews = Review.all
@@ -12,8 +11,8 @@ class ReviewsController < ApplicationController
   end
 
   def new
-    # @book = Book.find(params[:id])
-    if @review = Review.find_by(id:params[:id], user_id:current_user.id)
+    @book = Book.find(params[:book_id])
+    if @review = Review.find_by(book_id:@book.id, user_id:current_user.id)
       render 'reviews/edit'
     else
      @review = Review.new
@@ -21,57 +20,48 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = current_user.reviews.new(review_params)
+    @review = Reviews.new(review_params)
     if @review.save
+      #遷移先、動作確認すること
       redirect_to book_path(@book)
     else
       render 'reviews/new'
     end
-
-
-    # 変更①
-    # @book = Book.find_by(params[:id])
-    # @review = current_user.reviews.new(review_params.merge(book_id: @book.id))
-    # if @review.save
-    #   redirect_to book_path(@book)
-    # else
-    #   render 'reviews/new'
-    # end
-
-    #変更前コード
-    # @review = current_user.reviews.new(review_params)
-    # if @review.save
-    # #本の詳細ページにリダイレクトorトップページに遷移させる
-    # redirect_to root_url, notice: "読書行動文を提出しました。"
-    # else
-    #   render 'reviews/new'
-    # end
   end
 
   def edit
-    @review = current_user.reviews.find_by(id:params[:id])
   end
 
   def update
     @review = current_user.reviews.find(params[:id])
     if @review.update(review_params)
-    redirect_to root_url, notice:"読書行動文を更新しました。"
+       #遷移先、動作確認すること
+      redirect_to book_path(@book), notice:"読書行動文を更新しました。"
     else
       render 'reviews/edit'
     end
 
   end
-
-  def destroy
-    @review.destroy
-    flash[:notice] = "reviews deleted"
-    redirect_to request.referrer ||　review　　
-  end
+  
+  # 削除機能は、後で実装。
+  # def destroy
+  #   @review.destroy
+  #   flash[:notice] = "reviews deleted"
+  #   redirect_to request.referrer ||　review　　
+  # end
 
 
   private
     def review_params
-      params.require(:review).permit(:purpose,:memo,:plan_now,:plan_future,:spot_photo,:user_id)
+      params.require(:review).permit(
+        :purpose,
+        :memo,
+        :plan_now,
+        :plan_future,
+        :spot_photo,
+        :user_id
+        ).merge(book_id:params[:book_id])
+        .merge(user_id:current_user.id)
     end
 
     def correct_user
@@ -80,10 +70,9 @@ class ReviewsController < ApplicationController
       redirect_to root_url if @review.nil?
     end
     
-    # reviewをnewする際に必要なメソッド？ここの意味がわからない。
-    # def set_books
-    #   @book = Book.find(params[:book_id])
-    # end
+     def set_books
+       @book = Book.find(params[:book_id])
+     end
   
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   devise_for :users
   resources :users
-  resources :reviews, [:new, :create]
+  resources :books do
+    resources :reviews
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
