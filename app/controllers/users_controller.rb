17920d73dc587ce79074d42a7493d8a768538cc4
zenfumi <ys00878@gmail.com>
2020-09-29 23:39:04 +0900

    modify user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :edit, :update]
   def show
     @user = User.find(params[:id])
-    @reviews = @user.reviews.paginate(page: params[:page])
+    # @reviews = @user.reviews.paginate(page: params[:page])
+    @reviews = Review.where(user_id: @user.id)
   end
 
   def index
