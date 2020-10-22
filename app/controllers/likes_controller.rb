87e5c6f1ab6c6e529e007c5e1433c79be660a58c
densetsu <tab.23.yude@gmail.com>
2020-10-23 00:11:10 +0900

    いいね非同期(作業途中)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,14 @@
 class LikesController < ApplicationController
   def create
-    @book = Book.find_by(id:params[:id])
-    @review = Review.find_by(id:params[:id])
+    @book = Book.find_by(id:params[:book_id])
+    @review = Review.find_by(id:params[:review_id])
     @like = Like.new(user_id:current_user.id,review_id:params[:review_id])
-
     #後でリダイレクト先と文法の確認
     @like.save
     # redirect_to book_review_path(params[:book_id], params[:review_id])
     #  redirect_to ("/reviews/#{params[:review_id]}")
     # redirect_back(fallback_location: root_path)
+    @likes = Like.where(review_id:params[:review_id])
 
   end
 
