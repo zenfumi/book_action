33f06cf42ae9d857c1d0a6e53246661bacb6a94a
zenfumi <ys00878@gmail.com>
2020-10-24 22:44:38 +0900

    odify database

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require("channels")
 require('jquery')
 require('javascripts/users_book_tab.js')
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
