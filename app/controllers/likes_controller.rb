4ab25857b3acc8651442deb9807f9561ef83206e
zenfumi <ys00878@gmail.com>
2020-10-24 16:57:39 +0900

    define instance variable correctly

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,22 +1,17 @@
 class LikesController < ApplicationController
   def create
-    @book = Book.find_by(id:params[:id])
-    @review = Review.find_by(id:params[:id])
     @like = Like.new(user_id:current_user.id,review_id:params[:review_id])
-
-    #後でリダイレクト先と文法の確認
+    @review = Review.find(params[:review_id])
+    @book = @review.book
+    @likes = Like.where(review_id: @review.id)
     @like.save
-    # redirect_to book_review_path(params[:book_id], params[:review_id])
-    #  redirect_to ("/reviews/#{params[:review_id]}")
-    # redirect_back(fallback_location: root_path)
-
   end
 
   def destroy
-    @review = Review.find_by(id:params[:id])
     @like = Like.find_by(user_id: current_user.id,review_id:params[:review_id])
+    @review = Review.find(params[:review_id])
+    @book = @review.book
+    @likes = Like.where(review_id: @review.id)
     @like.destroy
-    # redirect_to book_review_path(params[:book_id], params[:review_id])
   end
-
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,6 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 require('javascripts/users_book_tab.js')
-require('app/views/likes/create.js.erb')
-require('app/views/likes/destoy.js.erb')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
