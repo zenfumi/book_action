b788b8867df0f03acf38b7ef8aebe6a254548e4e
zenfumi <ys00878@gmail.com>
2020-11-03 18:19:30 +0900

    fix layout and add side bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,7 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 require('javascripts/users_book_tab.js')
-
+require('javascripts/top_side_bar.js')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
