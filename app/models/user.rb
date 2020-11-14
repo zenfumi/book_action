63e34b68c6d82f2e4f341b24b0ce627d1e58fcfd
zenfumi <ys00878@gmail.com>
2020-11-14 20:39:43 +0900

    fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
-      user.password = SecureRandom.urlsafe_base64
-      user.comfirmed_at = Time.now　# Confirmable を使用している場合は必要
+    user.password = SecureRandom.urlsafe_base64
+    user.confirmed_at = Time.now # Confirmable を使用している場合は必要
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_133653) do
+ActiveRecord::Schema.define(version: 2020_11_13_061351) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -75,6 +75,10 @@ ActiveRecord::Schema.define(version: 2020_11_07_133653) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.string "image"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
