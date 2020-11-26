ba03940584a9fd34ca2ef64d34782393925c4051
zenfumi <ys00878@gmail.com>
2020-11-26 09:10:29 +0900

    fix app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
+end
 
