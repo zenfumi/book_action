72063b945b52e46545fa10ab755fdf05422af046
zenfumi <ys00878@gmail.com>
2020-11-29 14:22:38 +0900

    add comment function

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+
+def create
+  @comment = Comment.new(comment_params)
+  @comment.user_id = current_user.id
+  @comment.save
+  redirect_to book_review_path(params[:book_id],params[:review_id])
+end
+
+private
+def comment_params
+  params.require(:comment).permit(:content).merge(review_id: params[:review_id])
+end
+
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_061351) do
+ActiveRecord::Schema.define(version: 2020_11_28_115850) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2020_11_13_061351) do
     t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "review_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["review_id"], name: "index_comments_on_review_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -84,6 +94,8 @@ ActiveRecord::Schema.define(version: 2020_11_13_061351) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "reviews"
+  add_foreign_key "comments", "users"
   add_foreign_key "reviews", "books"
   add_foreign_key "reviews", "users"
 end
