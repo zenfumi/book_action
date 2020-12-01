2145d365e00bd115cce92d7f5aec47eb1ca8bedc
EC2 Default User <ec2-user@ip-172-31-21-15.us-east-2.compute.internal>
2020-12-01 09:31:53 +0000

    fix application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
+
+  def set_search
+    @search = Book.ransack(params[:q])
+    @search_books = @search.result
+  end
 
 
- 
+
   protected
 
   def configure_permitted_parameters
@@ -12,4 +18,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
-
+end
