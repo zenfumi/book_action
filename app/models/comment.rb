6aaaf6a91c78ce2259c7861fa8b3041e58901026
zenfumi <ys00878@gmail.com>
2021-01-10 23:01:54 +0900

    fix model and migration about validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :review
+  validates :content, presence: true
 end
--- db/migrate/20201128115850_create_comments.rb
+++ db/migrate/20201128115850_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.string :content
+      t.string :content, null: false
       t.references :user, null: false, foreign_key: true
       t.references :review, null: false, foreign_key: true
 
