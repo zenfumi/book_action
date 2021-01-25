9211955dbb3147bf70a34470b2fae75052e6d46c
zenfumi <ys00878@gmail.com>
2021-01-25 15:29:27 +0900

    add user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,74 +7,51 @@ RSpec.describe User, type: :model do
     context "データが条件を満たすとき" do
       let(:user) { build(:user) }
       it "保存できる",type: :doing do
-        expect(user.valid?).to eq true
+        expect(subject).to eq true
       end
     end
-    context "名前が空のとき" do
+    context "emailが空のとき" do
       let(:user) { build(:user, email: "") }
       it "エラーが発生する",type: :running do
-        expect(user.valid?).to eq false
+        expect(subject).to eq false
         expect(user.errors.messages[:email]).to include "を入力してください"
       end
     end
     context "パスワードが空のとき" do
       let(:user) { build(:user, password: "") }
       it "エラーが発生する",type: :walking do
-        expect(user.valid?).to eq false
+        expect(subject).to eq false
         expect(user.errors.messages[:password]).to include "を入力してください"
       end
     end
     context "usernameが空のとき" do
       let(:user) {build(:user, username: "")}
-      it "エラーが発生する",type: jogging do
-        expect(user.valid?).to eq false
+      it "エラーが発生する",type: :jogging do
+        expect(subject).to eq false
         expect(user.errors.messages[:username]).to include "を入力してください"
       end
     end
+    context "username が20文字以上である時" do
+      let(:user)  { build(:user,username: "a" * 21) }
+      it "エラーが発生する",type: :swimming do
+        expect(subject).to eq false
+        expect(user.errors.messages[:username]).to include "は20文字以内で入力してください"
+      end
+    end
+    context "emailがすでに存在する時は無効である" do
+      before { create(:user, email: "test@email.com") }
+      let (:user) { build(:user, email: "test@email.com")}
+      it "エラーが発生する",type: :jumpping do
+        expect(subject).to eq false
+        expect(user.errors.messages[:email]).to include "はすでに存在します"
+      end
+    end
+    context "profileが空の時" do
+      let(:user)  { build(:user,image: "") }
+      binding.pry
+      it "保存できる",type: :hopping do
+      expect(subject).to eq true
+    end
+    end
   end
 end
-
-# ””に気を付ける！！！
-#     context “emailが空のとき” do
-#       let(:user) { build(:user, email: “”) }
-#       it “エラーが発生する” do
-#           expect(subject).to eq false
-#           expect(user.errors.messages[:email]).to include “を入力してください”
-#       end
-#     end
-#     context “passwordが空のとき” do
-#       let(:user) { build(:user, password: “”) }
-#       it “エラーが発生する” do
-#         expect(subject).to eq false
-#         expect(user.errors.messages[:password]).to include “を入力してください”
-#       end
-#     end
-#     context “name が20文字以上である時” do
-#       let(:user)  { build(:user,name: “a” * 21) }
-#       it “エラーが発生する” do
-#         expect(subject).to eq false
-#         expect(user.errors.messages[:name]).to include “は20文字以内で入力してください”
-#       end
-#     end
-#     context “emailがすでに存在する時は無効である” do
-#       before { create(:user, email: “test@email.com”) }
-#       let (:user) { build(:user, email: “test@email.com”)}
-#       it “エラーが発生する” do
-#         expect(subject).to eq false
-#         expect(user.errors.messages[:email]).to include “はすでに存在します”
-#       end
-#     end
-#   end
-#     #未実装
-#     context “ユーザーが削除されたとき” do
-#       subject { user.destroy }
-#       let(:user) { create(:user) }
-#       before do
-#         create_list(:category, 2, user: user)
-#         create(:category)
-#       end
-#       it “そのユーザーのメッセージも削除される” do
-#         expect { subject }.to change { user.messages.count }.by(-2)
-#       end
-#     end
-# end
